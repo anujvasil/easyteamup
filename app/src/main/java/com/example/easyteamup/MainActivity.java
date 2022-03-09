4bfc29b3a29f1aafebd6db41ef41131c6ab2f4a4
anujvasil <avasil@usc.edu>
2022-03-09 10:58:03 -0800

    added account setup wireframe

--- app/src/main/java/com/example/easyteamup/MainActivity.java
+++ app/src/main/java/com/example/easyteamup/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.easyteamup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
     }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, account_setup.class);
+        startActivity(intent);
+    }
+
+
+
 }
