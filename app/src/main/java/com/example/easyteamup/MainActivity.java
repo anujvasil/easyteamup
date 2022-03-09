c870e6d408f9366e51f1f446af9742c0c371a189
anujvasil <avasil@usc.edu>
2022-03-09 11:05:10 -0800

    open creataccount activity only if email and password fields are non-empty with create account button

--- app/src/main/java/com/example/easyteamup/MainActivity.java
+++ app/src/main/java/com/example/easyteamup/MainActivity.java
@@ -4,21 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.content.Intent;
 import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    EditText email;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button2);
+        email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        password = (EditText) findViewById(R.id.editTextTextPassword);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openNewActivity();
+                if (!email.getText().toString().matches("") && !password.getText().toString().matches("")){
+                    openNewActivity();
+                }
+
             }
         });
     }
