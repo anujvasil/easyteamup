cad70bb8ef1b25b81af93748b1e900de00d8b077
anujvasil <avasil@usc.edu>
2022-03-28 22:18:10 -0700

    join button active on event_info page

--- app/src/main/java/com/example/easyteamup/event_info.java
+++ app/src/main/java/com/example/easyteamup/event_info.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class event_info extends AppCompatActivity {
     String username, email, fullname;
+    Button join;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,21 @@ public class event_info extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_info);
+
+        join = (Button) findViewById(R.id.button29);
+
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openList();
+
+            }
+        });
     }
+
+    public void openList(){
+        Intent intent = new Intent(this, discover_list.class);
+        startActivity(intent);
+    }
+
 }
